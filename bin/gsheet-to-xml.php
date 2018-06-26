4ba5935c43d8593e06b8fccfdafdd4a40f1d9045
John (Arcus) <john@arcuscompliance.com>
2018-06-26 09:41:34 +0100

    Find autoloader when standalone mode or as package.
    I also added some user friendly tip to encourage them to run 'composer install'
    as that is the most likely reason for not finding the autoloader.
    
    See: https://github.com/xmlsquad/gsheet-to-xml/issues/19

--- bin/gsheet-to-xml.php
+++ bin/gsheet-to-xml.php
@@ -1,6 +1,39 @@
 #!/usr/bin/env php
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+
+$autoloaderFound = FALSE;
+
+$possibleAutoloaderPaths = array(
+    __DIR__.'/../../../../vendor/autoload.php', //for when package is in vendor directory
+    __DIR__.'/../vendor/autoload.php' //for when bin is in root
+    );
+
+foreach ($possibleAutoloaderPaths as $autoloaderFile) {
+    if (@file_exists($autoloaderFile)) {
+        $autoloaderFound = TRUE;
+        break;
+    }
+}
+
+if ($autoloaderFound){
+    //Requie the autoloader
+    require_once $autoloaderFile;
+} else {
+
+    //tell the user in a friendly way:
+    fwrite(STDOUT, 'autoload.php not found.'. PHP_EOL);
+    fwrite(STDOUT, 'Searched in:'. PHP_EOL);
+
+    reset($possibleAutoloaderPaths);
+    foreach ($possibleAutoloaderPaths as $autoloaderFile) {
+        fwrite( STDOUT, '[' . $autoloaderFile . ']'. PHP_EOL);
+        }
+
+    fwrite(STDOUT, 'Maybe you forgot to run \'composer install\'?'. PHP_EOL);
+
+    exit(1);
+}
+
 
 use XmlSquad\GsheetXml\Command\GsheetToXmlCommand;
 use Symfony\Component\Console\Application;
