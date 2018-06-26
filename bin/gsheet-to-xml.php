d4df5c182c075330054955a40cd4b6aee70ca2cf
John (Arcus) <john@arcuscompliance.com>
2018-06-26 11:38:41 +0100

    reset() is not needed. STDERR is best output for error msg.
    Also refactored the code for clarity.
    
    See: https://github.com/xmlsquad/gsheet-to-xml/issues/19

--- bin/gsheet-to-xml.php
+++ bin/gsheet-to-xml.php
@@ -1,39 +1,41 @@
 #!/usr/bin/env php
 <?php
 
-$autoloaderFound = FALSE;
 
+//START - Autoloader inclusion
+
+//Determine if autoloader can be found.
 $possibleAutoloaderPaths = array(
-    __DIR__.'/../../../../vendor/autoload.php', //for when package is in vendor directory
-    __DIR__.'/../vendor/autoload.php' //for when bin is in root
+    __DIR__.'/../../../../vendor/autoload.php', //for when package bin is in vendor directory
+    __DIR__.'/../vendor/autoload.php' //for when standalone bin is in root
     );
 
+$autoloaderFound = FALSE;
 foreach ($possibleAutoloaderPaths as $autoloaderFile) {
-    if (@file_exists($autoloaderFile)) {
+    if (file_exists($autoloaderFile)) {
         $autoloaderFound = TRUE;
         break;
     }
 }
 
+//Either require autoloader.php or gracefully report error.
 if ($autoloaderFound){
-    //Requie the autoloader
     require_once $autoloaderFile;
 } else {
-
     //tell the user in a friendly way:
-    fwrite(STDOUT, 'autoload.php not found.'. PHP_EOL);
-    fwrite(STDOUT, 'Searched in:'. PHP_EOL);
+    fwrite(STDERR, 'autoload.php not found.'. PHP_EOL);
+    fwrite(STDERR, 'Searched in:'. PHP_EOL);
 
-    reset($possibleAutoloaderPaths);
     foreach ($possibleAutoloaderPaths as $autoloaderFile) {
-        fwrite( STDOUT, '[' . $autoloaderFile . ']'. PHP_EOL);
+        fwrite( STDERR, '[' . $autoloaderFile . ']'. PHP_EOL);
         }
 
-    fwrite(STDOUT, 'Maybe you forgot to run \'composer install\'?'. PHP_EOL);
+    fwrite(STDERR, PHP_EOL .'Maybe you forgot to run \'composer install\'?'. PHP_EOL . PHP_EOL);
 
+    //Exit with non-zero (i.e error) code.
     exit(1);
 }
-
+//END - Autoloader inclusion
 
 use XmlSquad\GsheetXml\Command\GsheetToXmlCommand;
 use Symfony\Component\Console\Application;
